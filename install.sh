75eeab6ff55e532345e07ec4e465d783bdc58b51
Stan <MisturDust319@users.noreply.github.com>
2021-08-10 18:56:23 -0400

    Initial Commit

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install git
+brew install --cask firefox
+brew install --cask visual-studio-code
+brew install --cask adobe-creative-cloud
+brew install --cask onedrive
